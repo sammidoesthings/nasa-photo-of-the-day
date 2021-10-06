42b14b64cdde1d2c7aa5e61e0a62810f8a36facd
Samantha <samanthaltl.sfh@gmail.com>
2021-10-06 13:08:43 -0700

    maximum call stack error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from "react";
 import "./App.css";
+import axios from 'axios';
+import {useState} from 'react'
+import {useEffect} from 'react'
+import {nasaURL, apiKey, keyConnect} from './getapod'
 
 function App() {
+
+  const [nasaData, setNasaData] = useState([]);
+  const [spacePhoto, setSpacePhoto] = useState('imgURL')
+
+  useEffect(() => {
+    axios.get(`${nasaURL}${keyConnect}${apiKey}`)
+      .then(response => {
+        setNasaData(response.data)
+        console.log(nasaData)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  },[]
+  )
+
+  console.log(nasaData);
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        <div> {spacePhoto}
+        </div>
       </p>
     </div>
   );
---
+++ src/getapod.js
@@ -0,0 +1,5 @@
+const nasaURL="https://api.nasa.gov/planetary/apod"
+const apiKey="i9xYSJJ4D8lRivrQDufZeQ5dRHRpZaOWHxq0ocK8"
+const keyConnect="?api_key="
+
+export {nasaURL, apiKey, keyConnect} from './getapod'
