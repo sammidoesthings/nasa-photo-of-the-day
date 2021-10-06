090ff005dfac6472f0f43d3b3fcabc20039337a8
Samantha <samanthaltl.sfh@gmail.com>
2021-10-06 14:28:39 -0700

    day 3 final project

--- src/App.js
+++ src/App.js
@@ -1,37 +1,48 @@
 import React from "react";
 import "./App.css";
 import axios from 'axios';
-import {useState} from 'react'
-import {useEffect} from 'react'
-import {nasaURL, apiKey, keyConnect} from './getapod'
+import {useState} from 'react';
+import {useEffect} from 'react';
+import {nasaURL, apiKey, keyConnect} from './getapod';
 
 function App() {
 
   const [nasaData, setNasaData] = useState([]);
-  const [spacePhoto, setSpacePhoto] = useState('imgURL')
 
   useEffect(() => {
     axios.get(`${nasaURL}${keyConnect}${apiKey}`)
       .then(response => {
         setNasaData(response.data)
-        console.log(nasaData)
+        return nasaData;
       })
       .catch(err => {
         console.error(err)
       })
   },[]
-  )
+  );
 
   console.log(nasaData);
 
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-        <div> {spacePhoto}
+
+      <p>Here's where you want stuff to show up!</p>
+      <p><span role="img" aria-label='go!'>🚀</span> NASA #APOD <span role="img" aria-label='go!'>🚀</span></p>
+      
+      <div className="apod-container"> 
+
+        <div className="apod-image">
+          <img src={`${nasaData.url}`} alt="spacephoto"/>
         </div>
-      </p>
+
+        <div className="apod-desc">
+          <p>date: {nasaData.date} | title: {nasaData.title}</p>
+          <p>photographer: {nasaData.copyright}</p> 
+          <p>explanation: {nasaData.explanation}</p>
+        </div>
+
+      </div>
+
     </div>
   );
 }
--- src/getapod.js
+++ src/getapod.js
@@ -2,4 +2,4 @@ const nasaURL="https://api.nasa.gov/planetary/apod"
 const apiKey="i9xYSJJ4D8lRivrQDufZeQ5dRHRpZaOWHxq0ocK8"
 const keyConnect="?api_key="
 
-export {nasaURL, apiKey, keyConnect} from './getapod'
+export {nasaURL, apiKey, keyConnect}
